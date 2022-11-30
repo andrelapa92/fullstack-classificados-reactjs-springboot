cc724a375dcdc8d95763eedeae9928d011fe1dd4
Andre Lapa <andrelapa92@gmail.com>
2022-11-30 01:39:17 -0300

    first commit

--- Java-Spring-React-StorageApp/src/main/java/com/ufsc/file/upload/exceptions/ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.ufsc.file.upload.exceptions;
-
-public class ProductNotFoundException extends RuntimeException {
-    
-    public ProductNotFoundException (Long id) {
-        super( "Não foi possível encontrar o produto " + id );
-    }
-    
-}
